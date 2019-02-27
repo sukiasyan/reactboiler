d2b885a6dc3b8c48a39fab1691471f27ce9d1e69
Hakob Sukiasyan <shakob@gmail.com>
2019-02-27 20:30:17 +0400

    Redux finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-    Switch,
-    Redirect
-  } from 'react-router-dom'
+import Footer from './components/Footer'
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList'
 import "./styles/style.scss";
-import Users from './components/fullRoaster'
-import Contact from './components/contact'
-import Home from './components/home'
-import Menu from './components/main'
 import Header from './components/header'
 import Main from './components/main'
 
@@ -22,21 +13,10 @@ const App = () => {
         <hr/>
         <Header />
         <Main />
+        <AddTodo />
+    <VisibleTodoList />
+    <Footer />
     </div>
   );
 };
 export default App;
-// ReactDOM.
-
-// import React from 'react'
-// import Header from './Header'
-// import Main from './Main'
-
-// const App = () => (
-//   <div>
-//     <Header />
-//     <Main />
-//   </div>
-// )
-
-// export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 import App from './App';
 
+const store = createStore(rootReducer)
+
 render((
+  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
 ), document.getElementById('app'));
